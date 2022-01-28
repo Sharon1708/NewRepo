5d527db2930f81982ae40948b3cd5b3f50799b97
Sharon1708 <sharonpriyanga7@gmail.com>
2022-01-28 15:38:22 +0530

    Sample

---
+++ src/test/java/org/infosys/git/project/MavenDayFiveGitProject/TestProgram.java
@@ -0,0 +1,14 @@
+package org.infosys.git.project.MavenDayFiveGitProject;
+
+public class TestProgram 
+{
+	public static void main(String[] args) 
+	{
+		int a= 10;
+		int b=3;
+		int c = a+b;
+		System.out.println("The addition of two numbers "+c);
+		
+	}
+
+}
