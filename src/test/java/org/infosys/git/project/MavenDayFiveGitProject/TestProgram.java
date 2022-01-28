b70908c7b4390ed147855a6a44cf5403f5c1dd4a
Sharon1708 <sharonpriyanga7@gmail.com>
2022-01-28 16:06:22 +0530

    new update

--- src/test/java/org/infosys/git/project/MavenDayFiveGitProject/TestProgram.java
+++ src/test/java/org/infosys/git/project/MavenDayFiveGitProject/TestProgram.java
@@ -9,6 +9,11 @@ public class TestProgram
 		int c = a+b;
 		System.out.println("The addition of two numbers "+c);
 		
+		int j=2;
+		int k=7;
+		int v=j+k;
+		System.out.println("The addition of new two numbers " + v);
+		
 	}
 
 }
